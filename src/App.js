a7055b0c4875cdaaedec7bc3bfff09e2db416c7b
charan <saicharantedla@gmail.com>
2023-10-14 21:58:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/navbar";
+import Intro from "./component/Intro/intro";
+import Skills from "./component/Skills/skills";
+import Works from "./component/Works/works";
+import Contact from "./component/contact/contact";
+import Footer from "./component/footer/footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Intro/>
+      <Skills />
+      <Works />
+      <Contact />
+      <Footer />
     </div>
   );
 }
